1b81bc2ccb96909f0ec33a3f3a158bf5b2276c38
deku-99 <75273987+deku-99@users.noreply.github.com>
2023-12-21 00:04:00 +0100

    Add HLD

--- Cpp/ALGO_LCA.cpp
+++ Cpp/ALGO_LCA.cpp
@@ -2,26 +2,26 @@ struct LCA
 {
     // Assuming number of nodes < (1<<19) ~ <=5e5
     vector<ll> tin, tout;
-    vector<vector<int>> parent;
+    vector<vector<ll>> parent;
 
-    void init(int root, vector<int> adj[], int n)
+    void init(ll root, vector<vector<ll>> &adj, ll n)
     {
         tin.assign(n + 1, 0);
         tout.assign(n + 1, 0);
-        parent.assign(n + 1, vector<int>(20, 0));
+        parent.assign(n + 1, vector<ll>(20, 0));
         vector<bool> visit(n + 1, false);
         ll timer = 0;
         dfs(root, adj, visit, timer);
-        for (int j = 1; j < 20; j++)
+        for (ll j = 1; j < 20; j++)
         {
-            for (int i = 1; i <= n; i++)
+            for (ll i = 1; i <= n; i++)
             {
                 parent[i][j] = parent[parent[i][j - 1]][j - 1];
             }
         }
     }
 
-    void dfs(ll node, vector<int> adj[], vector<bool> &visit, ll &timer)
+    void dfs(ll node, vector<vector<ll>> &adj, vector<bool> &visit, ll &timer)
     {
         tin[node] = timer++;
         visit[node] = true;
@@ -35,18 +35,18 @@ struct LCA
         }
         tout[node] = timer++;
     }
-    bool is_ancestor(int u, int v) // is u an ancestor of v
+    bool is_ancestor(ll u, ll v) // is u an ancestor of v
     {
         return (tin[u] <= tin[v] && tout[u] >= tout[v]);
     }
-    int lca(int u, int v)
+    ll lca(ll u, ll v)
     {
         if (is_ancestor(u, v))
             return u;
         if (is_ancestor(v, u))
             return v;
         // Upar jao, jabtak ancestor just milne na pae
-        for (int i = 19; i >= 0; --i)
+        for (ll i = 19; i >= 0; --i)
         {
             if (parent[u][i] != 0 && is_ancestor(parent[u][i], v) == false)
                 u = parent[u][i];
